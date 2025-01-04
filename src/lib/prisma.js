fe0a414e6642ff674185a59f5bc7bf34da6f71ea
Ozan Dundar (Hi-Tech Talents LLC) <ozn.dndr921@gmail.com>
2025-01-04 06:23:43 +0300

    add singleton approach for prisma

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,16 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
-let prisma
+const globalForPrisma = global
 
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient({
-      log: ['query', 'error', 'warn'],
-    })
-  }
-  prisma = global.prisma
-}
+export const prisma = 
+  globalForPrisma.prisma ?? 
+  new PrismaClient({
+    log: ['query'],
+  })
 
-export { prisma } 
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma 
